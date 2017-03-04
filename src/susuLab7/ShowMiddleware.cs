a8aaa704fb5369febe5df97e2ebd4cc885a45e40
Mazguhin <mazguhin.evgen@gmail.com>
2017-03-04 22:05:50 +0500

    Start

---
+++ src/susuLab7/ShowMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace susuLab7
+{
+    public class ShowMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ShowMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string path = context.Request.Path.Value.ToLower();
+            if (path == "/show")
+            {
+                if (context.Request.Query.ContainsKey("id"))
+                    await context.Response.WriteAsync("Passed value: " + context.Request.Query["id"]);
+                else
+                    await context.Response.WriteAsync("Value is missing");
+            }
+            else
+            {
+                await context.Response.WriteAsync("Page not found");
+            }
+        }
+    }
+}
